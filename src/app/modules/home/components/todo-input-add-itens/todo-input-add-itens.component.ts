e20dc83c3d122c0542126718016b256f87bc59d0
Gabriel Kayran <kayran@biomainvestimentos.com>
2024-03-12 14:01:11 -0300

    feat: Delete and add tasks work

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
-  styleUrls: ['./todo-input-add-itens.component.scss']
+  styleUrls: ['./todo-input-add-itens.component.scss'],
 })
 export class TodoInputAddItensComponent implements OnInit {
+  @Output() public emitItemTaskList = new EventEmitter();
 
-  constructor() { }
+  public addItemTaskList: string = '';
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public submitItemTaskList(): void {
+    this.addItemTaskList = this.addItemTaskList.trim();
+    if (this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = '';
+    }
+    return;
   }
-
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public setEmitTaskList(event: string): void {
+    this.taskList.push({ task: event, checked: false });
   }
 
+  public deleteItemTaskList(event: number): void {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(): void {
+    const confirm = window.confirm('Tem certeza que quer deletar tudo?');
+
+    if (!confirm) {
+      return;
+    }
+
+    this.taskList = [];
+  }
 }
